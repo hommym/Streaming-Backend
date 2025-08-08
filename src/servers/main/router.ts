606dece8d1bec4a2d8ad821540a9db7b31d84d79
hommym <herbertharthur80@gmail.com>
2025-08-08 10:47:26 +0000

    fix: update authRouter import to use getRouter method in mainServerRouter

--- src/servers/main/router.ts
+++ src/servers/main/router.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { authRouter } from "../../features/auth/authController";
+import { authController } from "../../features/auth/authController";
+
 
 
 
 
 export const mainServerRouter=Router()
 
-mainServerRouter.use("/auth",authRouter)
+mainServerRouter.use("/auth",authController.getRouter())
