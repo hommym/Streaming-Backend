1da11eb30c91cd2b16242b2b0cf5373028838c15
hommym <herbertharthur80@gmail.com>
2025-10-11 13:44:10 +0000

    Started SiteAgent Service for movie links retrival from specific sites.

---
+++ build/common/utils/services/siteAgentService.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.siteAgentService = void 0;
+class SiteAgentService {
+    learner() {
+    }
+    executer() {
+    }
+}
+exports.siteAgentService = new SiteAgentService();
+if (require.main === module) {
+    exports.siteAgentService.learner();
+}
---
+++ src/common/utils/services/siteAgentService.ts
@@ -0,0 +1,32 @@
+
+
+
+
+
+class SiteAgentService{
+
+
+
+
+
+    public learner(){
+
+    }
+
+
+
+
+    public executer(){
+
+    }
+
+
+}
+
+
+export const siteAgentService=new SiteAgentService();
+
+
+if(require.main===module){
+    siteAgentService.learner()
+}
